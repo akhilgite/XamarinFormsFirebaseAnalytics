0d128e4aa3b55bcd51bc8890d70b1b39d0e667a0
Akhil Gite <akhilgite@gmail.com>
2022-05-11 16:27:56 +0530

    First commit.

---
+++ FirebaseAnalytics/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirebaseAnalytics
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            IFirebaseAnalyticsService service = DependencyService.Get<IFirebaseAnalyticsService>();
+            service?.LogEvent("MyEventHere");
+        }
+    }
+}
